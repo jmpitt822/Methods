3f3415418f4307067a286d1bce2c2345a9e48091
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2016-09-04 17:23:35 -0500

    Cleaned-up code

---
+++ src/Project.java
@@ -0,0 +1,53 @@
+/**
+ * Created by jeremypitt on 8/30/16.
+ * Contains the name, due date and completion status of a particular project
+ */
+public class Project {
+    private String projName;
+    private String dueDate;
+    private boolean isFinished;
+    private boolean validDate;
+
+    public Project(String projName, String dueDate, boolean isFinished){
+        this.projName = projName;
+        this.dueDate = dueDate;
+        this.isFinished = isFinished;
+    }
+
+    public void setDueDate(String dueDate){
+        if (dueDate.contains("/")) {
+            this.dueDate = dueDate; //contains slashes
+        }
+    }
+
+    public String getDueDate(){
+        return dueDate;
+    }
+
+    public void dateIsValid(String dueDate){
+        if(dueDate.contains("2016")){
+            this.validDate = true;
+        }
+    }
+
+    public void setIsFinished(boolean isFinished){
+        if (validDate = true){
+            this.isFinished = isFinished;
+        }
+    }
+
+    public boolean getIsFinished(){
+        return isFinished;
+    }
+
+    public void setProjName(String projName){
+        if(projName.contains("Project")) {
+            this.projName = projName; //contains "Project"
+        }
+    }
+
+    public String getProjName(){
+        return projName;
+    }
+
+}
